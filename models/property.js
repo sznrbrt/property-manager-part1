2f67406a0045672234a15e8d2f32d94868592da3
Norbert Szucs <szucs.norbert@outlook.com>
2016-05-03 19:10:29 -0700

    full CRUD for properties on backend

--- models/property.js
+++ models/property.js
@@ -2,12 +2,15 @@
 
 var mongoose = require('mongoose');
 
-var Property = mongoose.model('Property', {
-  address: String,
-  status: String,
-  roomNum: Number,
-  rentPrice: Number,
-  utilCost: Number
+var propertySchema = new mongoose.Schema({
+  address: { type: String, required: true},
+  status: { type: String, required: true, default: 'N/A'},
+  roomNum: { type: Number, required: true},
+  rentPrice: { type: Number, required: true},
+  utilCost: { type: Number},
+  tenants: [{ type: String}]
 });
 
-module.exports = FlashCard;
+var Property = mongoose.model('Property', propertySchema);
+
+module.exports = Property;
--- routes/properties.js
+++ routes/properties.js
@@ -3,9 +3,40 @@
 var express = require('express');
 var router = express.Router();
 
+var Property = require('../models/property');
+
 // GET /api/properties
 router.get('/', (req, res) => {
-  res.send('Hello from /api/properties\n');
+  Property.find({}, (err, properties) => {
+    return err ? res.status(400).send(err) : res.send(properties);
+  });
 })
+
+// POST /api/properties
+router.post('/', (req, res) => {
+  var property = new Property(req.body);
+  property.save({new: true}, (err, property) => {
+    return err ? res.status(400).send(err) : res.send(property);
+  })
+})
+
+// DELETE /api/properties/:id
+router.delete('/:id', (req, res) => {
+  var id = req.params.id;
+  Property.findByIdAndRemove(id, (err) => {
+    if(err) return res.status(400).send(err);
+    else res.send(`id: ${id} Document deleted!`);
+  })
+})
+
+// PUT /api/property/:id
+router.put('/:id', (req, res) => {
+  var id = req.params.id;
+  Property.findByIdAndUpdate(id, { $set: req.body }, {new: true}, (err, property) => {
+    if(err) return res.status(400).send(err);
+    else res.send(property);
+  })
+})
+
 
 module.exports = router;
